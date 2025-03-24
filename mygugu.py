fa0762b4d6f1db0e4ef5d3aa3abdc08f5a2ab771
MINJU 207 <jmj040308@gmail.com>
2025-03-24 03:24:43 +0000

    덧셈 뺄셈 곱셈 나눗셈 연산

--- mygugu.py
+++ mygugu.py
@@ -1,13 +1,16 @@
 # 숫자 두개를 입력을 받아서
 # +, -, * / 를 출력 하는 프로그램을 만들어 보자
 
+# 사용자에게 숫자를 입력하도록 명령
 num1 = float(input("첫 번째 숫자를 입력하세요: "))
 num2 = float(input("두 번째 숫자를 입력하세요: "))
 
+# 덧셈, 뺄셈, 곱셈, 나눗셈을 수행하고 출력
 print(f"{num1} + {num2} = {num1 + num2}")
 print(f"{num1} - {num2} = {num1 - num2}")
 print(f"{num1} * {num2} = {num1 * num2}")
 
+# 결과 출력 단, 0으로 나눌 수 없도록 설정
 if num2 != 0:
     print(f"{num1} / {num2} = {num1 / num2}")
 else:
